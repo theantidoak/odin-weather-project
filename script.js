82b32b7ec3df3015981e0bf9069b7e3e7b2338c5
theantidoak <tcdoak@gmail.com>
2023-04-09 18:58:38 +0200

    set up weather api calls

--- script.js
+++ script.js
@@ -1,72 +1,102 @@
-const weatherForm = document.getElementById('weather-form');
-weatherForm.addEventListener("submit", submitForm)
+const weatherForm = (() => document.getElementById('weather-form'))();
+const box = (() => document.querySelector(".box"))();
+window.addEventListener("load", addFormListener)
+
+function addFormListener() {
+  weatherForm.addEventListener("submit", submitForm);
+}
 
 function loadScreenON() {
-  const box = document.querySelector(".box");
   box.classList.remove("load-off");
   box.classList.add("load-on");
 }
 
 function loadScreenOFF() {
-  const box = document.querySelector(".box");
   box.classList.remove("load-on");
   box.classList.add("load-off");
 }
 
-// Practice with Async/Await
-const getWeather = async (location) => { 
-  const APIkey = 'e9a6614f553731a73dd13cc17e5ff8d9';
-  const apiCall = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`);
-  const weatherData = await apiCall.json();
-  
-  return weatherData;
-};
+function renderGif(tempDepictionGif) {
+  const imageElement = document.createElement("img");
+  imageElement.src = tempDepictionGif;
+  imageElement.setAttribute('style', 'height: 100%; width: 100%;')
+  box.firstElementChild.replaceWith(imageElement);
+}
 
-// Practice with Promise
+const callGif = (tempDepiction) => {
+  const giphyKey = 'aeev1OHOzg63e4CLueZKdGec1jr2ZmDr';
+  const gifCall = Promise.resolve(fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${giphyKey}&s=${tempDepiction}&limit=1&weirdness=5`));
+  const gifData = gifCall.then((res) => res.json());
+  
+  gifData.then((res) => renderGif(res.data.images.original.url));
+}
+
+function handleGif(weatherData) {
+  if (weatherData.weather[0].description.split(" ").length > 1) {
+    return callGif(weatherData.weather[0].main);
+  }
+
+  return callGif(weatherData.weather[0].description);
+}
+
+function renderWeather(weatherData) {
+  console.log(weatherData);
+  console.log("Celcius", weatherData.main.temp_max - 273.15);
+  console.log("Fahrenheit", ((weatherData.main.temp_max - 273.15) * 1.8) + 32);
+}
+
+async function callAltCountries(city) {
+  const weatherKey = 'e9a6614f553731a73dd13cc17e5ff8d9';
+  const geoCall = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${weatherKey}`);
+  const geoData = await geoCall.json();
+
+  return geoData;
+}
+
+async function callWeather(location) {
+  const weatherKey = 'e9a6614f553731a73dd13cc17e5ff8d9';
+  const weatherCall = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${weatherKey}`);
+  const weatherData = await weatherCall.json();
+  const altLocations = await callAltCountries(weatherData.name);
+  if (altLocations.length > 1) {
+    return callWeatherByCoords(altLocations)
+  }
+
+  return weatherData;
+}
+
+async function callWeatherByCoords(altLocations) {
+  console.log(altLocations);
+  const lat = altLocations[0].lat;
+  const lon = altLocations[0].lon;
+  const weatherKey = 'e9a6614f553731a73dd13cc17e5ff8d9';
+  const weatherCall = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherKey}`);
+  const weatherData = await weatherCall.json();
+
+  return weatherData;
+}
+
+function handleFormSubmit() {
+  const inputValue = document.querySelector("input[type=search]").value;
+  
+  new Promise((resolve) => {
+    return setTimeout(() => resolve(callWeather(inputValue), 500));
+  })
+  .then((weatherData) => {
+    renderWeather(weatherData);
+    handleGif(weatherData);
+  })
+  .catch((error) => console.error(error))
+  .finally(() => {
+    weatherForm.reset();
+    loadScreenOFF();
+  })
+}
+
 function submitForm(e) {
   e.preventDefault();
   loadScreenON();
-  const searchInput = document.querySelector("input[type=search]")
-  const location = searchInput.value;
-  const weatherData = new Promise((resolve) => {
-    return setTimeout(() => resolve(getWeather(location)), 500);
-  });
-
-  weatherData.then((weatherData) => {
-    console.log(weatherData);
-    console.log("Celcius", weatherData.main.temp_max - 273.15);
-    console.log("Fahrenheit", ((weatherData.main.temp_max - 273.15) * 1.8) + 32);
-  }).catch((error) => {
-    console.error(error)
-  }).finally(() => loadScreenOFF())
+  handleFormSubmit();
 
   return false;
 }
-
-// Other Promise Options
-
-// With Async/Await
-// const weatherFetchAPI = async () => {
-//   await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`)
-//   .then(async (response) => weatherData = await response.json() )
-//   .catch((error) => console.error(error))
-
-//   console.log(weatherData);
-//   return weatherData;
-// };
-
-// Without Async/Await
-// const weatherResolvePromiseAPI = () => {
-//   const apiPromise = Promise.resolve(fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`));
-//   const apiResponse = apiPromise.then((response) => response.json())
-//   apiResponse.then((response) => console.log( response )).catch((error) => console.error(error));
-// };
-
-// const weatherNewPromiseAPI = () => {
-//   return new Promise((resolve) => {
-//     resolve(fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`))
-//   })
-//   .then((response) => response.json())
-//   .then((response) => console.log( response ))
-//   .catch((error) => console.error(error))
-// };
