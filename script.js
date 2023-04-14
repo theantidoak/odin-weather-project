9aa248af69a609b9cc82a953e00323ce53970c14
theantidoak <tcdoak@gmail.com>
2023-04-14 23:54:38 +0200

    add celcius and fahrenheit and fix alternate cities output

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const weatherForm = (() => document.getElementById('weather-form'))();
+const gifBtn = (() => document.querySelector(".gif-btn"))();
 const box = (() => document.querySelector(".box"))();
-const weatherSection = document.querySelector(".weather-info");
-const altSection = document.querySelector(".alt-locations");
-window.addEventListener("load", addFormListener)
+const weatherSection = (() => document.querySelector(".weather-info"))();
+const altSection = (() => document.querySelector(".alt-locations"))();
+const degreesBtn = (() => document.querySelector(".degrees"))();
+window.addEventListener("load", addEventHandlers)
 
-function addFormListener() {
+// Global
+
+function addEventHandlers() {
   weatherForm.addEventListener("submit", submitForm);
+  gifBtn.addEventListener("click", toggleGif);
+  degreesBtn.addEventListener("click", toggleDegrees);
 }
 
 function loadScreenON() {
@@ -16,6 +22,46 @@ function loadScreenON() {
 function loadScreenOFF() {
   box.classList.remove("load-on");
   box.classList.add("load-off");
+}
+
+function renderOtherMetric() {
+  const feelsLike = document.getElementById("feelsLike");
+  const temp = document.getElementById("temp");
+  const targetMetric = document.querySelector(".metric");
+  const feelsLikeTemp = feelsLike.textContent.replace(/\D/g, '');
+  const tempTemp = temp.textContent.replace(/\D/g, '');
+
+  feelsLike.textContent = targetMetric.id === "fahr" ? 
+    Math.round((+feelsLikeTemp - 32) * (5 / 9)) + "°C" :
+    Math.round((+feelsLikeTemp * (9 / 5)) + 32) + "°F";
+
+  temp.textContent = targetMetric.id === "fahr" ? 
+    Math.round((+tempTemp - 32) * (5 / 9)) + "°C" :
+    Math.round((+tempTemp * (9 / 5)) + 32) + "°F";
+}
+
+function toggleDegrees(e) {
+  renderOtherMetric();
+
+  const spans = e.currentTarget.querySelectorAll("span");
+  spans.forEach((span) => span.classList.toggle("metric"));
+}
+
+// Gif
+
+function toggleGif(e) {
+  const closedGif = e.currentTarget.textContent === "Show Gif" ? true : false;
+  closedGif ? showGif(e) : hideGif(e);
+}
+
+function showGif(e) {
+  box.style.display = "block";
+  e.currentTarget.textContent = "Hide Gif";
+}
+
+function hideGif(e) {
+  box.style.display = "none";
+  e.currentTarget.textContent = "Show Gif";
 }
 
 function renderGif(tempDepictionGif) {
@@ -41,29 +87,58 @@ function handleGif(weatherData) {
   return callGif(weatherData.weather[0].description);
 }
 
-function renderWeather(weatherData) {
-  console.log(weatherData);
-  console.log("Celcius", weatherData.main.temp_max - 273.15);
-  console.log("Fahrenheit", ((weatherData.main.temp_max - 273.15) * 1.8) + 32);
+// Cities with same names
+
+function deleteDuplicates(altLocations) {
+  return altLocations.filter((location, i, arr) => {
+    const states = arr.map((location) => location.state).indexOf(location.state);
+    const countries = arr.map((location) => location.country).indexOf(location.country);
+
+    if (!(states !== i && countries !== i)) {
+      return location;
+    } 
+  });
+}
+
+function updateSelectedCity(target) {
+  [...altSection.firstElementChild.children].forEach((div) => {
+    if (div === target) {
+      div.classList.add("selected");
+    } else {
+      div.classList.remove("selected");
+    }
+  })
+}
+
+function removeDuplicates(cardContainer) {
+  const duplicates = [...cardContainer.children].filter((card, i , arr) => i !== 0 && Math.round(card.lat) === Math.round(arr[0].lat));
+
+  if (duplicates.length < 1) {
+    return;
+  } else {
+    cardContainer.removeChild(cardContainer.firstElementChild);
+  }
 }
 
 function renderAltCountries(weatherData) {
-  console.log(weatherData);
-  for (let i = 1; i < weatherData.length; i++) {
+  const cardContainer = document.createElement('div');
+
+  for (let i = 0; i < weatherData.length; i++) {
     const card = document.createElement('div');
     const text = document.createElement('p');
-    const content = document.createTextNode(weatherData[i].name + ', ' + weatherData[i].country);
+    const loc = `${weatherData[i].name}${weatherData[i].state ? ', ' + weatherData[i].state : ''} (${weatherData[i].country ? weatherData[i].country : weatherData[i].sys.country})`;
+    const content = document.createTextNode(loc); 
     text.appendChild(content)
-    if (weatherData[i].state) {
-      const stateContent = document.createTextNode(` (${weatherData[i].state})`);
-      text.appendChild(stateContent);
-    }
     card.appendChild(text);
-    card.lat = weatherData[i].lat;
-    card.lon = weatherData[i].lon;
-    card.addEventListener('click', handleFormSubmit)
-    altSection.appendChild(card)
+    card.lat = i === 0 ? weatherData[i].coord.lat : weatherData[i].lat;
+    card.lon = i === 0 ? weatherData[i].coord.lon : weatherData[i].lon;
+    card.addEventListener('click', handleFormSubmit);
+    cardContainer.appendChild(card);
   }
+
+  removeDuplicates(cardContainer);
+  altSection.firstElementChild.replaceWith(cardContainer);
+  updateSelectedCity(cardContainer.firstElementChild);
 }
 
 async function callAltCountries(city) {
@@ -74,63 +149,115 @@ async function callAltCountries(city) {
   return geoData;
 }
 
-async function callWeatherByCoords(altLocations, otherLocation=false) {
-  const lat = otherLocation ? altLocations : altLocations[0].lat;
-  const lon = otherLocation ? otherLocation : altLocations[0].lon;
-  console.log("lan", lat)
-  console.log("lon", lon)
-  const weatherKey = 'e9a6614f553731a73dd13cc17e5ff8d9';
-  const weatherCall = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherKey}`);
-  const weatherData = await weatherCall.json();
-
-  renderAltCountries(altLocations);
-
-  return weatherData;
-}
-
-async function callWeather(location, e) {
-  const weatherKey = 'e9a6614f553731a73dd13cc17e5ff8d9';
-  const weatherCall = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${weatherKey}`);
-  const weatherData = await weatherCall.json();
+async function handleAltCountries(weatherData, cityCall) {
+  if (!cityCall) return;
   const altLocations = await callAltCountries(weatherData.name);
-  if (altLocations.length > 1) {
-    return callWeatherByCoords(altLocations);
+  const filteredLocations = deleteDuplicates(altLocations);
+
+  if (filteredLocations.length > 1) {
+    filteredLocations.unshift(weatherData);
+    renderAltCountries(filteredLocations);
   }
+}
+
+// Weather
+
+function createWeatherObject(args) {
+  const temp = { id: 'temp', val: args[0] };
+  const feelsLike = { id: 'feelsLike', val: args[1] };
+  const humidity = { id: 'humidity', val: args[2] };
+  const wind = { id: 'wind', val: args[3] };
+  const description = { id: 'description', val: args[4] };
+
+  return [temp, feelsLike, humidity, wind, description];
+}
+
+function addTextContent(...args) {
+  const objects = createWeatherObject(args);
+
+  for (let i = 0; i < objects.length; i++) {
+    const element = document.querySelector(`#${objects[i].id}`);
+    element.textContent = objects[i].val;
+  }
+}
+
+function clearAll() {
+  const emptyDiv = document.createElement("div");
+  altSection.firstElementChild.replaceWith(emptyDiv);
+  addTextContent("", "", "", "", "", "");
+}
+
+function convertToC(value) {
+  return Math.floor(value - 273.15) + "°C";
+}
+
+function convertToF(value) {
+  return Math.floor(((value - 273.15) * 1.8) + 32) + "°F";
+}
+
+function convertToMPH(value) {
+  return Math.floor(value * 2.237) + " MPH"
+}
+
+function capFirstLetter(description) {
+  return description.charAt(0).toUpperCase() + description.slice(1);
+}
+
+function renderWeather(weatherData, notInput, value) {
+  notInput ? updateSelectedCity(value) : null;
+
+  const targetMetric = document.querySelector(".metric");
+  const convert = targetMetric.id === "celc" ? convertToC : convertToF;
+  const temp = convert(weatherData.main.temp);
+  const feelsLike = convert(weatherData.main.feels_like);
+  const humidity = weatherData.main.humidity + "%";
+  const wind = convertToMPH(weatherData.wind.speed);
+  const description = capFirstLetter(weatherData.weather[0].description);
+  
+  addTextContent(temp, feelsLike, humidity, wind, description);
+}
+
+async function callWeather(...args) {
+  const weatherKey = 'e9a6614f553731a73dd13cc17e5ff8d9';
+  const cityCall = args[1] === 'city';
+  const cityAPI = `https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&appid=${weatherKey}`;
+  const coordsAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${args[0]}&lon=${args[1]}&appid=${weatherKey}`;
+  const weatherCall = cityCall ? await fetch(cityAPI) : await fetch(coordsAPI);
+  const weatherData = await weatherCall.json();
+
+  if (weatherData.cod != '200') {
+    clearAll();
+    throw weatherData.message;
+  }
+
+  handleAltCountries(weatherData, cityCall);
 
   return weatherData;
 }
 
-function handleWeather(e) {
-  const inputValue = document.querySelector("input[type=search]").value;
-  let promise;
-  if (e && e.currentTarget.parentElement === altSection) {
-    const coords = e.currentTarget;
-    promise = new Promise((resolve) => {
-      return setTimeout(() => resolve(callWeatherByCoords(coords.lat, coords.lon), 500));
-    })
-  } else {
-    promise = new Promise((resolve) => {
-      return setTimeout(() => resolve(callWeather(inputValue, e), 500));
-    })
-  }
+function handleWeather(notInput, value) {
+  const params = notInput ? [value.lat, value.lon] : [value, 'city'];
 
-  return promise;
+  return new Promise((resolve) => {
+    return setTimeout(() => resolve(callWeather.apply(null, params), 500));
+  })
 }
 
 function handleFormSubmit(e) {
+  const notInput = e && altSection.contains(e.currentTarget);
+  const value = notInput ? e.currentTarget : document.querySelector("input[type=search]").value;
+  const promise = handleWeather(notInput, value);
   
-  const promise = handleWeather(e);
-
   promise
-  .then((weatherData) => {
-    renderWeather(weatherData);
-    handleGif(weatherData);
-  })
-  .catch((error) => console.error(error))
-  .finally(() => {
-    weatherForm.reset();
-    loadScreenOFF();
-  })
+    .then((weatherData) => {
+      renderWeather(weatherData, notInput, value);
+      handleGif(weatherData);
+    })
+    .catch((error) => console.error(error))
+    .finally(() => {
+      weatherForm.reset();
+      loadScreenOFF();
+    });
 }
 
 function submitForm(e) {
